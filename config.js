3cdfe873e8798566b9b6d4da3b45ed9d1561319b
Noolayko <rawahankwanga148@gmail.com>
2024-05-29 23:32:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://te
 
 global.devs = "923192173398" // Developer Contact
 global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "null";
-global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "923192173398";
+global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "256760911417";
 
 
 
@@ -61,7 +61,7 @@ global.api_smd = "https://api-smd-1.vercel.app"
 global.scan = "https://myqr-43bb863fa5eb.herokuapp.com/";
 
 
-global.SESSION_ID = process.env.SESSION_ID ||  "" ;
+global.SESSION_ID = process.env.SESSION_ID ||  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQUhDVWpnMm1veWdodHJTVmZFZ2pYbzhJV1FUTkVDNmdraXlwd2xxT0Jsbz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoicXdDeElHUk1wbmZyQUxKd2FKdUltdDl6S2RzRFFuL1U3emx4d0JQTHUyND0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJxSDg1blR1SEhUOG80ZVJTRUJ1c2YxOWdhbXg3cFZQL05tODJoRmxoOEdZPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJmTWlyRnJlZGlXck9LMkR0WGg0SDBDR3RIbFRPTW4vRE0za2czNW1XVW5vPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Im1CaXRuRVBmSUd0aHhpbkJmK0hHZE15azg0V3B2N2h0VXJ5dDY4SGg1MUk9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjNwVjZQTFFUOGJ5QkluY2M1UDZmdjYvNDFISmJrYk84ZCs0M0ZwUHZVeDA9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoia0xmalk5VG01Kzd2SDVJVUN2TURhM1pjWVRmMVRUYzk1MW1xb1l0SzEwdz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoib1g1d2tGdTNBd0MycS9TSmR0dWRkNHZLWHFLZjB0UzNkdnZBc2RiekpEVT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkxYVmVhaWpZK29hWFNsdVN4VjNubnJDNC9mOElrd3Y1SlJRU3NyMGpWTG5KMVBhZTFTUThRL1d1elVhYkVpRmYzWlptOXpTeFdpTGZ1K1RQekVXZ0J3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTAwLCJhZHZTZWNyZXRLZXkiOiJPTzI0NXZBNVl5YjhzOE5mMGdrY0k0V2RIVEp3am96T1VPZ2lMNktUV2prPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiI3WGFkT2NlaVNiNllvQ2FJNDI4UW9BIiwicGhvbmVJZCI6IjgzODYzZjQwLTYxZDYtNGI4Yy05NjY4LWIxYWY3OGNlZjMzOSIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJzZEtEcHRrMGN0MmxWQ2dla0NwN1NiWnBUYUk9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoibUtyMnUrNUNNSXppYTJpbW84TldnQmpzbkhnPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IjFZQjlUNDJKIiwibWUiOnsiaWQiOiIyNTY3NjA5MTE0MTc6N0BzLndoYXRzYXBwLm5ldCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDUEhJN0l3QkVKeVUzcklHR0FJZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoibEZpQzdaK2Y1U3dHNk1HY2E0Y2lCK2U5V1Fvc0VSOUVrdTQvcmxWZmpFZz0iLCJhY2NvdW50U2lnbmF0dXJlIjoiSytUbnlNUnFwcjk1UzhrWUUra1ZSV1hFYTlWSlV3VXhwZiswYis0Zm9SRFExZ01jdCtZdjZVdDRIdzhQTW9Ddy91U1IxY2xzUXpWT2NCdWFEaTRMQkE9PSIsImRldmljZVNpZ25hdHVyZSI6Im05enJMTzFwVjlRV3BxcUZnOEZBVGgzUzh1UXErSHZySXBmcWo2dCt1aGoxQjhNbHVtdTRkWk96aXI4Z0Zoa2drWWRud1R2NWF5US9lN0VCbW9JRkNBPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiMjU2NzYwOTExNDE3OjdAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCWlJZZ3UyZm4rVXNCdWpCbkd1SElnZm52VmtLTEJFZlJKTHVQNjVWWDR4SSJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTcxNzAxMzAzMywibXlBcHBTdGF0ZUtleUlkIjoiQUFBQUFQc0YifQ==" ;
 
 
 module.exports = {
@@ -75,8 +75,8 @@ module.exports = {
  
   author : process.env.PACK_AUTHER|| "𝗪𝗔𝗦𝗜 ",
   packname: process.env.PACK_NAME || "♥️",
-  botname : process.env.BOT_NAME  || "𝗪𝗔𝗦𝗜 𝗠𝗗",
-  ownername:process.env.OWNER_NAME|| "It's 𝗜𝗧𝗫𝗫𝗪𝗔𝗦𝗜",
+  botname : process.env.BOT_NAME  || "DON HACKER",
+  ownername:process.env.OWNER_NAME|| "It's DON HACKER",
 
 
   errorChat : process.env.ERROR_CHAT || "",
